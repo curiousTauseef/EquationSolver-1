960d842e143dd2b6aeb7aff5c0c5490bcd607353
kim366 <kim.schmider@hotmail.com>
2017-03-03 18:07:37 +0100

    Separate Equations into left and right

--- Input.cpp
+++ Input.cpp
@@ -12,20 +12,33 @@ std::istream& operator>>(std::istream& is_, Input& obj_)
 	// Find out if character is separating opeator (+/-)
 	auto isseparatingoperator = [] (const char char_)
 	{
-		return (char_ == '+' || char_ == '-');
+		return (char_ == '+' || char_ == '-' || char_ == '=');
 	};
 
 	// Prepend + Symbol
-	if (!isseparatingoperator(input.front()))
-		input.insert(0, 1, '+');
+	auto prependplus = [&] ()
+	{
+		if (!isseparatingoperator(input.front()))
+			input.insert(0, 1, '+');
+	};
+	
+	prependplus();
 
 	// Split Input String into _expression vector while keeping preceeding separating operators
 	auto found = input.begin();
+	auto* current_container = &obj_._left_expressions;
 	while (found != input.end())
 	{
-		found = std::find_if(input.begin() + 1, input.end(), isseparatingoperator);
+		if (input.front() == '=')
+		{
+			current_container = &obj_._right_expressions;
+			input.erase(0, 1);
+			prependplus();
+			continue;
+		}
 
-		obj_._left_expressions.emplace_back(input.begin(), found);
+		found = std::find_if(input.begin() + 1, input.end(), isseparatingoperator);
+		current_container->emplace_back(input.begin(), found);
 		input.erase(input.begin(), found);
 		found = input.begin();
 	}
